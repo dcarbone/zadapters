6c3654a138df94798a6966fb0ae0ede362d6f9dd
Daniel Carbone <daniel.p.carbone@gmail.com>
2019-04-01 09:57:04 -0500

    updating zstdlogger to use most-recent DefautlLevel value if one is not specifified, and updating tests to include console output

--- zstdlog/adapter.go
+++ zstdlog/adapter.go
@@ -8,6 +8,7 @@ import (
 
 type adapter struct {
 	level zerolog.Level
+	def   bool
 	log   zerolog.Logger
 }
 
@@ -17,21 +18,20 @@ var (
 
 // NewStdLoggerWithLevel will return an instance of *log.Logger where all messages will have the specified level
 func NewStdLoggerWithLevel(logger zerolog.Logger, level zerolog.Level) *stdlog.Logger {
-	return stdlog.New(adapter{level, logger}, "", 0)
+	return stdlog.New(adapter{level, false, logger}, "", 0)
 }
 
 // NewStdLogger will return an instance of *log.Logger where all messages will have no level attached
 func NewStdLogger(logger zerolog.Logger) *stdlog.Logger {
-	return NewStdLoggerWithLevel(logger, DefaultLevel)
+	return stdlog.New(adapter{DefaultLevel, false, logger}, "", 0)
 }
 
-func (a adapter) Write(p []byte) (n int, err error) {
-	n = len(p)
-	if n > 0 && p[n-1] == '\n' {
-		p = p[0 : n-1]
+func (a adapter) Write(p []byte) (int, error) {
+	if a.def {
+		return a.WriteLevel(DefaultLevel, p)
+	} else {
+		return a.WriteLevel(a.level, p)
 	}
-	a.log.WithLevel(a.level).Msg(string(p))
-	return
 }
 
 func (a adapter) WriteLevel(level zerolog.Level, p []byte) (int, error) {
--- zstdlog/adapter_test.go
+++ zstdlog/adapter_test.go
@@ -9,8 +9,11 @@ import (
 )
 
 var (
-	expectedStdLoggerNoLevel = []byte("{\"message\":\"test logger\"}\n")
-	expectedStdLoggerLevel   = []byte("{\"level\":\"info\",\"message\":\"test logger with level\"}\n")
+	expectedStdLoggerNoLevelJSON = []byte("{\"message\":\"test logger\"}\n")
+	expectedStdLoggerLevelJSON   = []byte("{\"level\":\"info\",\"message\":\"test logger with level\"}\n")
+
+	expectedStdLoggerNoLevelConsole = []byte("<nil> %!S test logger\n")
+	expectedStdLoggerLevelConsole   = []byte("<nil> INF test logger with level\n")
 )
 
 type capture struct {
@@ -25,26 +28,89 @@ func newCapture(chlen int) *capture {
 
 func (c capture) Write(p []byte) (n int, err error) {
 	c.w <- p
-	return
+	return len(p), nil
 }
 
 func TestStdLogger(t *testing.T) {
-	w := newCapture(100)
+	t.Parallel()
 
-	l := zerolog.New(w)
+	t.Run("JSON", func(t *testing.T) {
+		w := newCapture(1)
+		defer close(w.w)
+		l := zerolog.New(w)
 
-	lg := zstdlog.NewStdLogger(l)
-	lg.Println("test logger")
+		lg := zstdlog.NewStdLogger(l)
+		lg.Println("test logger")
 
-	if msg := <-w.w; !bytes.Equal(msg, expectedStdLoggerNoLevel) {
-		t.Logf("%q does not match expected %q", string(msg), string(expectedStdLoggerNoLevel))
-		t.FailNow()
-	}
+		if len(w.w) != 1 {
+			t.Fatalf("Expected writer chan to have 1 message, but it has %d messages", len(w.w))
+		}
 
-	lgl := zstdlog.NewStdLoggerWithLevel(l, zerolog.InfoLevel)
-	lgl.Println("test logger with level")
-	if msg := <-w.w; !bytes.Equal(msg, expectedStdLoggerLevel) {
-		t.Logf("%q does not match expected %q", string(msg), string(expectedStdLoggerLevel))
-		t.FailNow()
-	}
+		if msg := <-w.w; !bytes.Equal(msg, expectedStdLoggerNoLevelJSON) {
+			t.Logf("%q does not match expected %q", string(msg), string(expectedStdLoggerNoLevelJSON))
+			t.FailNow()
+		}
+
+		if len(w.w) > 0 {
+			t.Fatalf("Expected writer chan to be empty, but it has %d elements", len(w.w))
+		}
+
+		lgl := zstdlog.NewStdLoggerWithLevel(l, zerolog.InfoLevel)
+		lgl.Println("test logger with level")
+
+		if len(w.w) != 1 {
+			t.Fatalf("Expected writer chan to have 1 message, but it has %d messages", len(w.w))
+		}
+
+		if msg := <-w.w; !bytes.Equal(msg, expectedStdLoggerLevelJSON) {
+			t.Logf("%q does not match expected %q", string(msg), string(expectedStdLoggerLevelJSON))
+			t.FailNow()
+		}
+
+		if len(w.w) > 0 {
+			t.Fatalf("Expected writer chan to be empty, but it has %d elements", len(w.w))
+		}
+	})
+
+	t.Run("Console", func(t *testing.T) {
+		w := newCapture(1)
+		defer close(w.w)
+
+		l := zerolog.New(zerolog.NewConsoleWriter(func(writer *zerolog.ConsoleWriter) {
+			writer.NoColor = true
+			writer.Out = w
+		}))
+
+		lg := zstdlog.NewStdLogger(l)
+		lg.Println("test logger")
+
+		if len(w.w) != 1 {
+			t.Fatalf("Expected writer chan to have 1 message, but it has %d messages", len(w.w))
+		}
+
+		if msg := <-w.w; !bytes.Equal(msg, expectedStdLoggerNoLevelConsole) {
+			t.Logf("%q does not match expected %q", string(msg), string(expectedStdLoggerNoLevelConsole))
+			t.FailNow()
+		}
+
+		if len(w.w) > 0 {
+			t.Fatalf("Expected writer chan to be empty, but it has %d elements", len(w.w))
+		}
+
+		lgl := zstdlog.NewStdLoggerWithLevel(l, zerolog.InfoLevel)
+		lgl.Println("test logger with level")
+
+		if len(w.w) != 1 {
+			t.Fatalf("Expected writer chan to have 1 message, but it has %d messages", len(w.w))
+		}
+
+		if msg := <-w.w; !bytes.Equal(msg, expectedStdLoggerLevelConsole) {
+			t.Logf("%q does not match expected %q", string(msg), string(expectedStdLoggerLevelConsole))
+			t.FailNow()
+		}
+
+		if len(w.w) > 0 {
+			t.Fatalf("Expected writer chan to be empty, but it has %d elements", len(w.w))
+		}
+	})
 }
